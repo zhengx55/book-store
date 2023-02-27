2bd3fbec0deca174844736d892dbdf2cf1e17ee5
zhengx55 <williamzhxin@gmail.com>
2023-02-27 12:33:42 +0800

    project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { CommonServerOptions, defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import fs from "fs";
+import dotenv, { DotenvParseOutput } from "dotenv";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig((mode) => {
+  const curMode: string = mode.mode;
+  const curFileName: string = `.env.${mode.mode}`;
+  let server: CommonServerOptions = {};
+  const envData = fs.readFileSync(curFileName);
+  const envMap: DotenvParseOutput = dotenv.parse(envData);
+  // if (curMode === "development") {
+  //   server = {
+  //     host: "127.0.0.1/",
+  //     port: 5000,
+  //     proxy: {
+  //       "/dang": {
+  //         target: "http://127.0.0.1:5173/",
+  //       },
+  //     },
+  //   };
+  //   console.log("development mode", server);
+  // }
+  return {
+    plugins: [vue()],
+    server,
+  };
+});
